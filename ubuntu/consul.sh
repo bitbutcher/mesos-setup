1bf112547b542514071dd4d344243feebbdee8f9
Mark DeSpain <mdespain@doubledutch.me>
2015-07-16 13:34:40 -0700

    more refinements

--- ubuntu/consul.sh
+++ ubuntu/consul.sh
@@ -48,7 +48,7 @@ do
   CONF_DIR="/etc/consul.d/${ROLE}"
   sudo mkdir -p "${CONF_DIR}"
   cp "${SCRIPT_DIR}/consul/${ROLE}.json" "${CONF_DIR}/config.json"
-  sed -i -e "s/{{datacenter}}/${DATACENTER}/" -e "s/{{key}}/${CONSUL_KEY}/" -e "s/{{cluster}}/${JOIN_CLUSTER}/" "${CONF_DIR}/config.json"
+  sed -i -e "s|{{datacenter}}|${DATACENTER}|" -e "s|{{key}}|${CONSUL_KEY}|" -e "s|{{cluster}}|${JOIN_CLUSTER}|" "${CONF_DIR}/config.json"
 done
 
 # setup the data directory
@@ -69,16 +69,16 @@ if [ "${CONSUL_ROLE}" == "client" ]; then
   rm "${CONSUL_WEB_ZIP}"
 
   # set the role of the upstart service to 'client'
-  sed -i -e "s/{{role}}/client/" "/etc/init/consul.conf"
+  sed -i -e "s|{{role}}|client|" "/etc/init/consul.conf"
 else
   # set the role of the upstart service to server
-  sed -i -e "s/{{role}}/server/" "/etc/init/consul.conf"
+  sed -i -e "s|{{role}}|server|" "/etc/init/consul.conf"
 fi
 
 # conditionally bootstrap the consul cluster
 if [ "${CONSUL_ROLE}" == "bootstrap" ]; then
 sudo su consul <<SUB
-timout 5 consul agent -config-dir /etc/consul.d/bootstrap
+timeout 5 consul agent -config-dir /etc/consul.d/bootstrap
 SUB
 fi
 
