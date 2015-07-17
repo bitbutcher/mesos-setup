8f309e769035b5d06a0bb5a585d8d5b9c8089a55
Sean M. Duncan <bitbutcher@gmail.com>
2015-07-17 09:48:36 -0700

    even more thorough

--- ubuntu/consul/cleanup.sh
+++ ubuntu/consul/cleanup.sh
@@ -3,9 +3,9 @@ sudo service consul stop
 
 # clean up files
 sudo rm -rf /var/lib/consul
-sudo rm /usr/local/bin/consul
+sudo rm -rf /usr/local/bin/consul
 sudo rm -rf /etc/consul.d/
-sudo rm /etc/init/consul.conf
+sudo rm -rf /etc/init/consul.conf
 sudo rm -rf /usr/share/consul
 
 # remove the consul user
